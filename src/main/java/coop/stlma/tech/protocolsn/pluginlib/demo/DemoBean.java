42abc13167e65a5b52a3ed73f84f9690786afa42
John <patrick.meyerin@gmail.com>
2024-05-26 12:41:54 -0500

    New skeleton micronaut lib

---
+++ src/main/java/coop/stlma/tech/protocolsn/pluginlib/demo/DemoBean.java
@@ -0,0 +1,11 @@
+package coop.stlma.tech.protocolsn.pluginlib.demo;
+
+import jakarta.inject.Singleton;
+
+@Singleton
+public class DemoBean {
+
+    public String hello() {
+        return "Hello World!";
+    }
+}
